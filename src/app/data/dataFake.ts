9bb182adf940d79a6878d488718f536a4426f42c
viniaquino <contato.viniaquino@outlook.com>
2023-05-15 21:32:12 -0300

    conteudos finalizados

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO",
+    "description":"marvel anuncia novo filme inédito",
+    "photoCover":"https://th.bing.com/th/id/OIP.08bGE4YPB9q_OZ9hS45YpgHaGm?w=188&h=180&c=7&r=0&o=5&pid=1.7"
+  },
+  {
+    "id":"2",
+    "title":"lançamento do novo filme",
+    "description":"blaskoasnsn",
+    "photoCover":"https://th.bing.com/th/id/R.2dd0ac7c8f4eb0bc21e5dab831c09668?rik=awMaefstovjSug&pid=ImgRaw&r=0"
+  }
+]
